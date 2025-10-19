8be5ee8b0fefdf36271e3c1f73619f7652bfeebe
leansan93 <sanchezleandro988@gmail.com>
2025-10-19 01:59:11 -0300

    backtracking

--- Tp3_Sudoku/src/model/Backtracking.java
+++ Tp3_Sudoku/src/model/Backtracking.java
@@ -7,15 +7,15 @@ public class Backtracking {
 	public boolean resolverSudoku( Sudoku sudoku ) {
 		int [][] tablero = sudoku.getTablaSudoku();
 		
-		for (int fila = 0 ; fila < 9 ; fila ++ ) {
-			
-			for (int col = 0 ; col < 9 ; col ++) {
+		for (int fila = 0 ; fila < 9 ; fila ++ ) { // recorre todas las filas 
+			 
+			for (int col = 0 ; col < 9 ; col ++) { // recorre todas las columnas 
 				if ( tablero[fila] [col] == 0 ) { // si es vacio la zelda 
-					for (int num = 1 ; num < 9 ; num ++) { // recorres del 
-						if (sudoku.esValido(fila, col, num)) { // si esvalido lo puesto entre 1 a 9 
+					for (int num = 1 ; num < 9 ; num ++) { // recorres del  1 al 9 para luego ver 
+						if (sudoku.esValido(fila, col, num)) { // si es esvalido lo puesto entre 1 a 9 
 							 tablero[fila][col]= num ; // lo agrega 
 							 
-							 if ( resolverSudoku(sudoku)) { // recursion 
+							 if (resolverSudoku(sudoku)) { // recursion 
 								 
 								 return true;
 								 
